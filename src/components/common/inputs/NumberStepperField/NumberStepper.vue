69bd6c016304b180ded6e82516cbe7066bdc90ee
Kęstutis Bučys <kestutis.bucys@codeopera.com>
2024-06-17 20:57:43 +0300

    Checkbox story & NumberStepperField (wip)

---
+++ src/components/common/inputs/NumberStepperField/NumberStepper.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="rc-number-stepper">
+    <v-container>
+      <v-row no-gutters>
+        <v-col class="rc-ns-col-input">
+          <v-text-field class="rc-text-field" variant="outlined" hide-details></v-text-field>
+
+        </v-col>
+        <v-col class="rc-ns-col-actions">
+          <v-btn>Test</v-btn>
+          <v-btn>Test</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script setup lang="ts">
+import './NumberStepperStyle.scss'
+</script>
