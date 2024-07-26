4f0175b71ca93fb482ca8c36ed45a590c6ba55a4
Vytautas Luksys <vytautas.luksys@cgi.com>
2024-07-26 15:45:55 +0300

    fix: phone input type patch

--- src/types/inputs/PhoneInputFieldProps.ts
+++ src/types/inputs/PhoneInputFieldProps.ts
@@ -1,6 +1,10 @@
 import type { TextFieldProps } from './TextFieldProps'
 
-export interface PhoneInputFieldProps
-  extends /* @vue-ignore */ Omit<TextFieldProps, 'placeholder'> {
+// export interface PhoneInputFieldProps
+//   extends /* @vue-ignore */ Omit<TextFieldProps, 'placeholder'> {
+//   defaultIso?: string
+// }
+
+export type PhoneInputFieldProps = Omit<TextFieldProps, 'placeholder'> & {
   defaultIso?: string
 }
