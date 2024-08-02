f747dd65f85e393e557737a7f278d068726472f8
Kęstutis Bučys <kestutis.bucys@codeopera.com>
2024-08-02 14:32:34 +0300

    code quality

--- src/components/examples/tabbedForm/ExampleTabbedForm.vue
+++ src/components/examples/tabbedForm/ExampleTabbedForm.vue
@@ -2,14 +2,14 @@
   <TabbedPanel
     title="test 123"
     :items="[
-        { title: 'Fizinis asmuo', value: 0, component: UzsakymasTabForm },
-        { title: 'Juridinis asmuo', value: 1, component: UzsakymasTabForm },
-        { title: 'Advokatų, antstolių kontora, notarų biuras', value: 2, active: true },
-        { title: 'Advokatų biuras', value: 3 },
-        { title: 'Notarų biuras', value: 4 },
-        { title: 'Biuras', value: 5 },
-        { title: 'Siuras', value: 6 },
-      ]"
+      { title: 'Fizinis asmuo', value: 0, component: UzsakymasTabForm },
+      { title: 'Juridinis asmuo', value: 1, component: UzsakymasTabForm },
+      { title: 'Advokatų, antstolių kontora, notarų biuras', value: 2, active: true },
+      { title: 'Advokatų biuras', value: 3 },
+      { title: 'Notarų biuras', value: 4 },
+      { title: 'Biuras', value: 5 },
+      { title: 'Siuras', value: 6 },
+    ]"
   >
     <template #actions>
       <FormActions />
@@ -19,9 +19,8 @@
       <FormActionsAfter />
     </template>
   </TabbedPanel>
-
 </template>
 <script setup lang="ts">
-import UzsakymasTabForm from '@/components/examples/tabbedForm/UzsakymasTabForm.vue'
 import FormActionsAfter from '@/components/examples/shared/FormActionsAfter.vue'
+import UzsakymasTabForm from '@/components/examples/tabbedForm/UzsakymasTabForm.vue'
 </script>
